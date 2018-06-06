17fec8aa034d53c621f26a3f89302720cce39470
sophiacyk <37350321+sophiacyk@users.noreply.github.com>
2018-06-06 13:54:20 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "oWOg2XYznRvF5HsuEzgi9nyUO"
+consumer_secret = "Tb5nADNCjBmiEkEW2YLZ7hK2l4nJFuMMdxYB1AaOwo7ZuNLc4T"
+access_token = "389023553-IAeuyUMghQjv3fvMHp4rF5UiK3dfWfd3Plrki7jG"
+access_token_secret = "m5Q7vSgiP3D1kxTy1eV1bR9RN7JGq3vrAhr8tz9qN2fwV"
